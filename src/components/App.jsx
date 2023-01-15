f223f3342c8bff22e297248c30541deddd5838de
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-01-15 23:25:13 +0200

    \\\

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import Statistics from "./statistics/statistics";
 import  FriendList  from "./friendlist/friendList";
 import TransactionHistory from "./history-tran/history-tran"
 
-import user from './path/user.json'
-import data from './path/data.json';
-import friends from './path/friends.json';
-import transactions from './path/transactions.json';
+import user from 'path/to/user.json';
+import data from 'path/to/data.json';
+import friends from 'path/to/friends.json';
+import transactions from 'path/to/transactions.json';
 export const App = () => {
   return (
     <div>
